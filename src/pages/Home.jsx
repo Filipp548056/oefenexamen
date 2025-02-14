a006a7cbf2b4f22092b54f0c8b7bfed655b228c0
Filipp548056 <548056@edu.rocmn.nl>
2025-02-14 17:28:38 +0100

    ad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,70 +7,83 @@ import Container from "react-bootstrap/Container"
 const Home = () => {
   return (
     <>
-      
-        <Container>
-          <Row>
-            <Col>
-              <h1>home page</h1>
-              <img src="/server-rack.jpg" width="100%"/>
-            </Col>
-          </Row>
 
-        </Container>
+      <Container>
+        <Row>
+          <Col>
+            <h1>home page</h1>
+            <img src="/server-rack.jpg" width="100%" />
+          </Col>
+        </Row>
 
-        <Container className="text-center">
-          <Row>
-            <Col>
+      </Container>
+
+      <Container className="text-center">
+        <Row>
+          <Col>
             <h1>
               YOUR PARTNER IS RELIABLE SCALEBLE GOSTING SOLUTIONSSSSSS
             </h1>
-            </Col>
-          </Row>
+          </Col>
+        </Row>
 
-          <Row>
-            <Col width="100px">
+        <Row>
+          <Col width="100px">
             <p width="100px">
               asdsadsasadsaddbdfhjshsahfjdshfjkahfjkshfdskjfh
               sdafhdsafjkdshfjkhsfjsahfjdsahfkjdshfkjdshfksdjahf
               dsfgdsafgsahjfgsdahjfgshajfgsadhjfgdsahkjfgdhsajfgas
               fsadfdsafdsafsdfsdfafdsafsafsafdsafdsafsafdsfdsfsdfsdfsd
             </p>
-            </Col>
-            <Col>
-            <img src="/computer-monitor-in-datacenter.jpg" width="300px"></img>
-            </Col>
-          </Row>
-        </Container>
-
-        <Container>
-          <Row>
-            <Col>
-            <h2>LIghtying fast nigger</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            <h2>expert support</h2>
-            <p>dfsdfsdafsafsadfdasf</p>
-            </Col>
-            <Col>
-            <h2>Scable solutions</h2>
-            </Col>
-            <Col>
-            <h2>asdsad</h2>
-            </Col>
-          </Row>
+          </Col>
           <Col>
-        
-            </Col>
-            <Col>
-            </Col>
-          <Row>
+            <img src="/computer-monitor-in-datacenter.jpg" width="300px"></img>
+          </Col>
+        </Row>
+      </Container>
 
-          </Row>
-        </Container>
+      <Container className="py-5 text-center bg-light">
+        <Row>
+          <Col>
+            <h2>LIghtying fast ndfds</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="mx-auto">
+            <Row className="justify-content-center">
+              <h2>expert support</h2>
+              <p>dfsdfsdafsafsadfdasf</p>
+            </Row>
+          </Col>
+          <Col>
+            <h2>Scable solutions</h2>
+            <p>dfsdfsdafsafsadfdasf</p>
+          </Col>
+          <Col>
+            <h2>asdsad</h2>
+            <p>dfsdfsdafsafsadfdasf</p>
+          </Col>
+        </Row>
+        <Row className="py-5 text-center bg-light">
+        <Col className="width: 500px" >
+        <h2>sadsadsa</h2>
+        <p>asdasd</p>
+        </Col>
+        <Col>
+        <h2>sadsad</h2>
+        <p>dsfgdsafgsahjfgsdahjfgshajfgsadhjfgdsahkjfgdhsajfgas</p>
+        </Col>
+        </Row>
+        <Row className="py-5 text-center bg-light">
+          <Col>
+          <button>Get Started Today</button>
+          </Col>
 
-      
+
+        </Row>
+      </Container>
+
+
 
     </>
   );
