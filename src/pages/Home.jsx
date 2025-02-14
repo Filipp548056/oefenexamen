325c581aa2cf48808e3e30ee7fdbee19fef7e1c8
Filipp548056 <548056@edu.rocmn.nl>
2025-02-14 16:32:47 +0100

    a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,61 @@ const Home = () => {
           <Row>
             <Col>
               <h1>home page</h1>
-              <img src="/server-rack.jpg" width="100%" />
+              <img src="/server-rack.jpg" width="100%"/>
             </Col>
+          </Row>
+
+        </Container>
+
+        <Container className="text-center">
+          <Row>
+            <Col>
+            <h1>
+              YOUR PARTNER IS RELIABLE SCALEBLE GOSTING SOLUTIONSSSSSS
+            </h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col width="100px">
+            <p width="100px">
+              asdsadsasadsaddbdfhjshsahfjdshfjkahfjkshfdskjfh
+              sdafhdsafjkdshfjkhsfjsahfjdsahfkjdshfkjdshfksdjahf
+              dsfgdsafgsahjfgsdahjfgshajfgsadhjfgdsahkjfgdhsajfgas
+              fsadfdsafdsafsdfsdfafdsafsafsafdsafdsafsafdsfdsfsdfsdfsd
+            </p>
+            </Col>
+            <Col>
+            <img src="/computer-monitor-in-datacenter.jpg" width="300px"></img>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container>
+          <Row>
+            <Col>
+            <h2>LIghtying fast nigger</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <h2>expert support</h2>
+            <p>dfsdfsdafsafsadfdasf</p>
+            </Col>
+            <Col>
+            <h2>Scable solutions</h2>
+            </Col>
+            <Col>
+            <h2>asdsad</h2>
+            </Col>
+          </Row>
+          <Col>
+        
+            </Col>
+            <Col>
+            </Col>
+          <Row>
+
           </Row>
         </Container>
 
